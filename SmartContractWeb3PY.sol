fc50630f90bcb72bc9b69ca4fe3834fa6bb15f52
Mohammad Hussam Alzeyyat <66519488+mhzcyber@users.noreply.github.com>
2022-09-15 00:48:07 +0300

    Create SmartContractWeb3PY.sol

---
+++ SmartContractWeb3PY.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract Greeter{
+    string public greeting;
+
+    constructor(){
+        greeting = "Hello";
+    }
+
+    function setGreeting(string memory _greeting) public{
+        greeting = _greeting;
+    }
+
+    function greet() view public returns(string memory){
+
+        return greeting;
+    }
+
+
+
+}
